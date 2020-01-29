a86769a40a0f1d8a62d6d9549c4f9e68060bb8ff
Dylan <dylan.allen@voicefoundry.com>
2020-01-28 20:30:54 -0600

    Added intial route handling and styling for site load

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,31 +1,39 @@
 import './style.css'
-import { useReducer, createContext } from 'react'
-import { stateTypes } from '../interfaces'
+import { useReducer, useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
 import { appReducer, initialState, initReducer } from '../utils/reducer'
-
-interface ctsType {
-  state: stateTypes;
-  dispatch: any
-}
-
-const stateCtx: ctsType = {
-  state: initialState,
-  dispatch: null
-}
-
-const StateContext = createContext(stateCtx);
 
 import * as React from "react";
 import { Container } from "next/app";
 
 function CApp(Props: any): any {
+  const router = useRouter()
+  const [containerClass, setCClass] = useState('hidden mainContain');
+  useEffect(() => {
+    if (window.location.pathname !== '/') {
+      router.push(`${window.location.pathname}${window.location.search}`)
+    }
+    setCClass('visible mainContain');
+  }, [])
   const { Component, pageProps } = Props;
   const [state, dispatch] = useReducer(appReducer, initialState, initReducer);
   return (
     <Container>
-      <StateContext.Provider value={{ state: state, dispatch: dispatch }}>
+      <div className={containerClass}>
         <Component state={state} dispatch={dispatch} {...pageProps} />
-      </StateContext.Provider>
+        <style jsx>
+        {
+          `.mainContain {
+            opacity: 1;
+            transition: all 0.4s ease 0.2s;
+          }
+
+          .mainContain.hidden {
+            opacity: 0;
+          }`
+        }
+        </style>
+      </div>
     </Container>
   )
 }
